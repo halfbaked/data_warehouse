1048d83a292e486db2edd293a6cf7c96f0a96f76
halfbaked <eamonnoconnell@gmail.com>
2021-09-21 22:49:14 +0200

    method rename

--- src/main/kotlin/com/erratick/Mappers.kt
+++ src/main/kotlin/com/erratick/Mappers.kt
@@ -5,14 +5,14 @@ import java.math.BigDecimal
 import java.math.RoundingMode
 
 fun FluxRecord.toQueryResult(measurement: Measurement) = QueryResult(
-    value = setScaleIfBigDecimal(value),
+    value = setScaleIfDouble(value),
     time = time,
     dimensions = values.filter { it.key in  measurement.dimensions }.map {
         it.key to it.value.toString()
     }.toMap()
 )
 
-fun setScaleIfBigDecimal(value: Any?): Any? {
+fun setScaleIfDouble(value: Any?): Any? {
     return if(value != null && value is Double)
                 BigDecimal(value).setScale(5, RoundingMode.DOWN)
             else value
